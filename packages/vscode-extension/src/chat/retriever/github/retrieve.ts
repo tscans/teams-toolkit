3c0a3ebc9c2c39b7eec205d70e66a7dcd85195e5
Long Hao <haolong@microsoft.com>
2024-06-24 18:03:59 +0800

    feat: github retriever

---
+++ packages/vscode-extension/src/chat/retriever/github/retrieve.ts
@@ -0,0 +1,90 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import fetch from "node-fetch";
+import { Issue } from "./types";
+
+export interface GithubIssue {
+  search(repo: string, q: string): Promise<GithubIssueSearchResult>;
+}
+
+export interface GithubIssueSearchResult {
+  items: Issue[];
+}
+
+export interface Github {
+  issue: GithubIssue;
+}
+
+export class GithubRetriever implements Github {
+  private static instance: GithubRetriever;
+  private token: string;
+
+  // TODO: @Long refine how to penetrate octokit to subs
+  issue: GithubIssue;
+
+  private constructor(token: string) {
+    this.token = token;
+    this.issue = new GithubIssueRetriever(this.token);
+  }
+
+  public reAuth(token: string): GithubRetriever {
+    GithubRetriever.instance = new GithubRetriever(token);
+    return GithubRetriever.instance;
+  }
+
+  public static getInstance(token: string): GithubRetriever {
+    if (!GithubRetriever.instance) {
+      GithubRetriever.instance = new GithubRetriever(token);
+    }
+    return GithubRetriever.instance;
+  }
+}
+
+class GithubIssueRetriever implements GithubIssue {
+  private token: string;
+
+  constructor(token: string) {
+    this.token = token;
+  }
+
+  async search(repo: string, query: string): Promise<GithubIssueSearchResult> {
+    const url = `https://api.github.com/search/issues?q=${encodeURIComponent(
+      query
+    )}+repo:${encodeURIComponent(repo)}`;
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `token ${this.token}`,
+        Accept: "application/vnd.github.v3+json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const data: GithubIssueSearchResult = await response.json();
+
+    // Optionally, fetch additional details for each issue
+    for (const issue of data.items) {
+      if (issue.comments == 0) {
+        continue;
+      }
+      // For comments, make an additional request
+      const commentsResponse = await fetch(issue.comments_url, {
+        method: "GET",
+        headers: {
+          Authorization: `token ${this.token}`,
+          Accept: "application/vnd.github.v3+json",
+        },
+      });
+
+      if (commentsResponse.ok) {
+        issue.fetchedComments = await commentsResponse.json();
+      }
+    }
+
+    return data;
+  }
+}
---
+++ packages/vscode-extension/src/chat/retriever/github/types.ts
@@ -0,0 +1,108 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export interface User {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+}
+
+export interface Milestone {
+  url: string;
+  html_url: string;
+  labels_url: string;
+  id: number;
+  node_id: string;
+  number: number;
+  title: string;
+  description: string;
+  creator: User;
+  open_issues: number;
+  closed_issues: number;
+  state: string;
+  created_at: string;
+  updated_at: string;
+  due_on: string | null;
+  closed_at: string | null;
+}
+
+export interface Label {
+  id: number;
+  node_id: string;
+  url: string;
+  name: string;
+  color: string;
+  default: boolean;
+  description: string;
+}
+
+export interface Reactions {
+  url: string;
+  total_count: number;
+  "+1": number;
+  "-1": number;
+  laugh: number;
+  hooray: number;
+  confused: number;
+  heart: number;
+  rocket: number;
+  eyes: number;
+}
+
+export interface Issue {
+  url: string;
+  repository_url: string;
+  labels_url: string;
+  comments_url: string;
+  events_url: string;
+  html_url: string;
+  id: number;
+  node_id: string;
+  number: number;
+  title: string;
+  user: User;
+  labels: Label[];
+  state: string;
+  locked: boolean;
+  assignee: User;
+  assignees: User[];
+  milestone: Milestone;
+  comments: number;
+  fetchedComments: Comment[];
+  created_at: string;
+  updated_at: string;
+  closed_at: null | string;
+  author_association: string;
+  active_lock_reason: null | string;
+  body: string;
+  reactions: Reactions;
+  timeline_url: string;
+  state_reason: null | string;
+  score: number;
+}
+
+export interface Comment {
+  id: number;
+  node_id: string;
+  user: User;
+  created_at: string;
+  updated_at: string;
+  author_association: string;
+  body: string;
+  reactions: Reactions;
+}
