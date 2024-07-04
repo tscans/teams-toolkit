68f6aff6507fce73be55939a3bbc4b1fe9bc4f48
Yimin-Jin <yiminjin@microsoft.com>
2024-07-04 15:00:35 +0800

    test: test

--- packages/tests/src/e2e/caseFactory.ts
+++ packages/tests/src/e2e/caseFactory.ts
@@ -184,6 +184,8 @@ export abstract class CaseFactory {
           expect(result).to.be.true;
           process.env["AZURE_RESOURCE_GROUP_NAME"] = appName + "-rg";
 
+          console.log("this.options: " + this.options);
+
           const { success } = await Executor.provision(
             projectPath,
             "dev",
